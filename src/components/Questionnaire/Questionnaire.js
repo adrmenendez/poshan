c7a616459523b9c9e24f69fc4313b888a7df8c6e
adrmenendez <adrmenendez@gmail.com>
2021-06-10 09:26:55 +0200

    add some data

--- src/components/Questionnaire/Questionnaire.js
+++ src/components/Questionnaire/Questionnaire.js
@@ -26,3 +26,9 @@ class Questionnaire extends React.Component {
 }
 
 export default Questionnaire;
+
+//como usamos el data
+//con el peso y la altura calculamos el IMC y vamos a definición o volumen
+//con todas las preguntas de deporte, en función de cuantas seleccione de cada categoría vamos a 'super', 'active', 'moderate' o 'lazy'
+//Dentro de cada actividad, número de comidas que se quieren hacer
+//Limpiamos la dieta con los alimentos que no nos gustan o a los que somos intolerantes
